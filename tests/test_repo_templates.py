194526c6d76cfd7bf714fe7e1c5dcbdfba799dd7
anatolii <tolik1992s@gmail.com>
2026-08-31 20:14:59 +0300

    Calibrate Predict adverse move reserve

--- tests/test_repo_templates.py
+++ tests/test_repo_templates.py
@@ -201,6 +201,6 @@ def test_production_services_use_bounded_concurrency_and_safe_exit_policy() -> N
         "polymarket_sx": 0.00025,
     }
     assert quote["spread_policy"]["adverse_move_p95_pct_by_route"] == {
-        "polymarket_predict": 0.005,
+        "polymarket_predict": 0.02,
         "polymarket_myriad": 0.05,
     }
